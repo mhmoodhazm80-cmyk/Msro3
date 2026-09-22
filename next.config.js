e816674d969d8725d080b47e5564ff7c3ec1dc44
Vip_x7x <mhmoodhazm80@gmail.com>
2026-09-22 22:00:23 +0300

    fix: server-rendered hero + no next-intl + no framer-motion: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-const createNextIntlPlugin = require('next-intl/plugin');
-const withNextIntl = createNextIntlPlugin('./src/i18n.js');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compress: true,
-  poweredByHeader: false,
   images: {
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
   },
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
