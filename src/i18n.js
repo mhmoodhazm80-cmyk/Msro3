91c0237725afd5b10c36cd5cc9b1af9cf5d32e9d
Vip_x7x <mhmoodhazm80@gmail.com>
2026-09-22 20:47:58 +0300

    Add files via upload

--- src/i18n.js
+++ src/i18n.js
@@ -6,5 +6,7 @@ export const defaultLocale = 'ar-EG';
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale)) notFound();
-  return { messages: (await import(`./messages/${locale}.json`)).default };
+  return {
+    messages: (await import(`./messages/${locale}.json`)).default,
+  };
 });
