0cd01942e8cf899aeb6c9e11b17b6d85725b0f0a
Vip_x7x <mhmoodhazm80@gmail.com>
2026-09-22 21:02:46 +0300

    fix: upgrade next + next-intl and fix static rendering: next.config.js

--- next.config.js
+++ next.config.js
@@ -6,15 +6,8 @@ const nextConfig = {
   reactStrictMode: true,
   compress: true,
   poweredByHeader: false,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
   images: {
     formats: ['image/avif', 'image/webp'],
-    deviceSizes: [375, 640, 768, 1024, 1280, 1536, 1920, 2560],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  experimental: {
-    optimizePackageImports: ['lucide-react', 'framer-motion'],
   },
 };
 
