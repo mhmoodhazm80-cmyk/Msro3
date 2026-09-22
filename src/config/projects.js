d03e288feb03d0dd6bdfe3495290d7850835e631
Vip_x7x <mhmoodhazm80@gmail.com>
2026-09-22 20:29:47 +0300

    رفع المشروع من سكربت Python: src/config/projects.js

---
+++ src/config/projects.js
@@ -0,0 +1,7 @@
+export const projects = [
+  { slug: 'rabaq',    image: '/images/projects/rabaq.jpg',    url: 'https://rabaq.co/' },
+  { slug: 'sahem',    image: '/images/projects/sahem.jpg',    url: 'https://sahem.shop/' },
+  { slug: 'vaneersa', image: '/images/projects/vaneersa.jpg', url: 'https://vaneersa.com/ar' },
+  { slug: 'lajoie',   image: '/images/projects/lajoie.jpg',   url: 'https://lajoie7.com/' },
+  { slug: 'kidsrus',  image: '/images/projects/kidsrus.jpg',  url: 'https://kidsrus.fun/' },
+];
